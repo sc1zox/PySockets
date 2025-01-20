bab3e76d28a6794b9a30e24a7bc5eb351a17d011
Christian Ingermann <christian-manuel.ingermann@stud.uni-bamberg.de>
2025-01-21 00:29:22 +0100

    multiplex

--- server.py
+++ server.py
@@ -1,28 +1,22 @@
 import socket
-import select
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(('127.0.0.1', 4712))
 
 print("UDP Server läuft auf Port 4712")
 
-socket_list = [sock]
-
 while True:
     try:
-        read_sockets,_,_ = select.select(socket_list,[],[])
-
-        for socket in read_sockets:
-            if socket == sock:
-                data, addr = sock.recvfrom(4096)
-                decoded_data = data.decode('utf-8')
-                print(f"Empfangen von {addr}: {decoded_data}")
+        data, addr = sock.recvfrom(4096)
+        decoded_data = data.decode('utf-8')
+        print(f"Empfangen von {addr}: {decoded_data}")
 
 
-                response_message = "Pong"
-                response = response_message.encode('utf-8')
-                sock.sendto(response, addr)
+        response_message = "Pong"
 
+
+        response = response_message.encode('utf-8')
+        sock.sendto(response, addr)
     except KeyboardInterrupt:
         print("\nServer wird geschlossen.")
         break
