151a7f36c4170b871a4203869247001e45eeb32f
Christian Ingermann <christian-manuel.ingermann@stud.uni-bamberg.de>
2025-01-21 00:27:53 +0100

    multiplex

--- client.py
+++ client.py
@@ -1,24 +1,46 @@
-import socket,time
+import socket
+import time
+import select
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def run_client(server_address):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.settimeout(2)
 
-server_address = ('127.0.0.1', 4712)
-start = time.time()
-sock.settimeout(2)
-try:
     message = "Ping"
-    print(f"Sende: {message}")
+    print(f"Sende: {message} an {server_address}")
 
     sock.sendto(message.encode('utf-8'), server_address)
+    return sock
 
-    data, server = sock.recvfrom(4096)
-    end = time.time()
-    elapsed = end - start
-    print(f"Antwort vom Server: {data.decode('utf-8')}\nAntwort nach {elapsed} Sekunden erhalten")
+server_address = ('127.0.0.1', 4712)
 
-except socket.timeout:
-    print('REQUEST TIMED OUT')
+sockets = []
+responses = {}
 
-finally:
-    print("Client closing")
-    sock.close()
+
+for i in range(5):
+    sock = run_client(server_address)
+    sockets.append(sock)
+    responses[sock] = {'start': time.time()}
+
+print("Warte auf Antworten von Servern...")
+
+
+while sockets:
+
+    readable, _, _ = select.select(sockets, [], [], 2)
+
+    for sock in readable:
+        try:
+            data, server = sock.recvfrom(4096)
+            end = time.time()
+            elapsed = end - responses[sock]['start']
+            print(f"Antwort vom Server: {data.decode('utf-8')}\nAntwort nach {elapsed:.4f} Sekunden erhalten")
+            sockets.remove(sock)
+            sock.close()
+        except socket.timeout:
+            print(f"REQUEST TIMED OUT für {sock.getpeername()}")
+            sockets.remove(sock)
+            sock.close()
+
+print("Alle Clients haben ihre Anfragen abgeschlossen.")
